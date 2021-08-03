f26e7e49c59ba2e38dea2579eb20a21c5cea868b
DanielFengHere <2537912f@student.gla.ac.uk>
2021-08-03 23:08:09 +0800

    add user models

--- apis/models.py
+++ apis/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
