045c8aff1ee71692f884f64490a0d06af22f759b
DanielFengHere <2537912f@student.gla.ac.uk>
2021-08-04 14:28:00 +0800

    add register models

--- apis/admin.py
+++ apis/admin.py
@@ -1,2 +1,6 @@
 from django.contrib import admin
-from apis.models import UserProfile
+from apis.models import UserProfile, Category, Movie
+
+admin.site.register(Category)
+admin.site.register(Movie)
+admin.site.register(UserProfile)
