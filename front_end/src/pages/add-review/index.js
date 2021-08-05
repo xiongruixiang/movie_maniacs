1c1dd290194bbc8639cc754f8e13b4787e60f088
RUIXIANG XIONG <2540725x@student.gla.ac.uk>
2021-08-05 23:06:41 +0100

    add likes and add review api

--- front_end/src/pages/add-review/index.js
+++ front_end/src/pages/add-review/index.js
@@ -1,104 +1,101 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './index.css'
-import { Form, Space, Rate, Input, Button } from 'antd';
+import {Form, Space, Rate, Input, Button, Select} from 'antd';
+import Header from "../../component/header";
+import $ from 'jquery'
 
 const desc = ['terrible', 'bad', 'normal', 'good', 'wonderful'];
+
+const {Option} = Select;
 
 export default class AddReview extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            value: 0,
+            message: 'Please select a rating',
+            showMessage: false
+        }
     }
 
 
-    // formRef = React.createRef();
     onFinish = (values) => {
-      console.log(values);
+        let username = localStorage.getItem('username')
+        if (username) {
+            if (this.state.value === 0) {
+                this.setState({
+                    showMessage: true
+                })
+            } else {
+                let {movie_name} = this.props.location.state
+                let url = 'http://127.0.0.1:8000/api/movie/' + movie_name + '/add-review/'
+                let params = {
+                    content: values.movie.review,
+                    username: username,
+                    rating: this.state.value
+                }
+                $.post(url, params, (result) => {
+                    console.log(result)
+                })
+            }
+        }
     };
-    // onReset = () => {
-    //   this.formRef.current.resetFields();
-    // };
+
 
     handleChange = value => {
-      this.setState({ value });
+        this.setState({value});
     };
 
-  render() {
+    render() {
 
-    const { value } = this.state;
+        const {value, showMessage, message} = this.state;
 
-    return (
+        return (
+            <div>
+                <Header/>
+                <Form
+                    name="addReview"
+                    className="add-review"
+                    onFinish={this.onFinish}
+                >
 
-      <Form
-      // ref={this.formRef}
-      name="addReview"
-      className="add-review"
-      onFinish={this.onFinish}
-      >
+                    <br/>
+                    <div className="add-rate-header">
+                        Rating
+                        {showMessage && <span>{message}</span>}
+                    </div>
 
-        <br/>
-        <div className="add-rate-header">
-          Rating
-        </div>
+                    <Form.Item
+                        name={['movie', 'rating']}
+                    >
+                        <Space>
+                            <Rate tooltips={desc} onChange={this.handleChange} value={value}/>
+                            {value ? desc[value - 1] : ''}
+                        </Space>
+                    </Form.Item>
 
-        <Form.Item
-        name={['movie', 'rate']}
-        >
-          <Space>
-            <Rate tooltips={desc} onChange={this.handleChange} value={value} />
-            {value ? desc[value - 1] : ''}
-          </Space>
-        </Form.Item>
+                    <div className="add-review-header">
+                        Review
+                    </div>
 
-        <div className="add-review-header">
-          Review
-        </div>
+                    <Form.Item
+                        name={['movie', 'review']}
+                        rules={[{required: true, message: 'Review cannot be empty!'}]}
+                    >
+                        <Input.TextArea rows={10} placeholder="Write a review..."/>
+                    </Form.Item>
 
-        <Form.Item
-        name={['movie', 'review']}
-        rules={[{ required: true, message: 'Review cannot be empty!' }]}
-        >
-          <Input.TextArea rows={10} placeholder="Write a review..." />
-        </Form.Item>
+                    <Form.Item className="add-review-button">
+                        <Space>
+                            <Button type="primary" htmlType="submit" className='submit-btn'>
+                                Submit
+                            </Button>
+                        </Space>
+                    </Form.Item>
 
-        <Form.Item className="add-review-button">
-          <Space>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-            <Button htmlType="button" href="/movie-detail">
-              Cancel
-            </Button>
-          </Space>
-        </Form.Item>
-
-      </Form>
-
-      // <div className='add-review'>
-
-      //   <div className='add-review-header'>
-      //     <br/>
-      //     <Space>
-      //       Rating
-      //       <Rate tooltips={desc} onChange={this.handleChange} value={value} />
-      //       {value ? desc[value - 1] : ''}
-      //     </Space>
-      //     <br/>
-      //     Review
-      //   </div>
-
-      //   <TextArea rows={10} placeholder="Write a review..." />
-
-      //   <div className='add-review-button'>
-      //     <br/>
-      //     <Space size="large">
-      //       <Button type="primary">Submit</Button>
-      //       <Button type="primary">Cancel</Button>
-      //     </Space>
-      //   </div>
-
-      // </div>
-    );
-  }
+                </Form>
+            </div>
+        );
+    }
 }
--- front_end/src/pages/movie-detail/index.js
+++ front_end/src/pages/movie-detail/index.js
@@ -1,24 +1,13 @@
-import React, { Component, createElement } from 'react';
+import React, {Component, createElement} from 'react';
 import './index.css'
 import Header from '../../component/header'
-import { Image, Descriptions, Space, Rate, Card, Button, Tooltip, List, Comment, Avatar } from 'antd';
-import imagesMap from '../../constants/images-map'
-import { LikeOutlined, LikeFilled } from '@ant-design/icons';
+import {Image, Descriptions, Space, Rate, Card, Button, Tooltip, List, Comment, Avatar, message} from 'antd';
+import {LikeOutlined, LikeFilled} from '@ant-design/icons';
+import $ from 'jquery'
 
-const data = [
-  {
-    user : 'user1',
-    rating : 3,
-    content : 'Good',
-    likes : 0
-  },
-  {
-    user : 'user2',
-    rating : 1,
-    content : 'Good',
-    likes : 1
-  }
-];
+const error = () => {
+    message.error('Please login');
+};
 
 export default class MovieDetail extends Component {
 
@@ -27,103 +16,144 @@ export default class MovieDetail extends Component {
         this.state = {}
     }
 
-    addLikes(num) {
-        console.log(num)
+    addLikes(index, movie_name) {
+        let username = localStorage.getItem('username')
+        if (username) {
+            let slug_name = movie_name.toLowerCase().split(' ').join('-')
+            let likes_element = document.getElementsByClassName('likes-number')[index]
+            let new_likes = Number(likes_element.innerHTML) + 1
+            likes_element.innerHTML = new_likes
+            let url = 'http://127.0.0.1:8000/api/movie/' + slug_name + '/add-likes/'
+            let params = {
+                new_likes: new_likes,
+                movie_name: movie_name,
+                review_index: index
+            }
+            $.post(url, params, (result) => {
+                console.log(result)
+                if (result.status === 200) {
+                    this.props.history.push({
+                        pathname: '/movie/' + slug_name,
+                        state: {
+                            movie_name: slug_name
+                        }
+                    })
+                }
+            })
+        } else {
+            error()
+        }
+    }
+
+    goToAddReviewPage(movie_name) {
+        let username = localStorage.getItem('username')
+        if (username) {
+            let pathname = '/movie/' + movie_name + '/add-review'
+            this.props.history.push({
+                pathname: pathname,
+                state: {
+                    movie_name: movie_name
+                }
+            })
+        } else {
+            error()
+        }
     }
 
 
     render() {
-
-        console.log(this.props.location.state.movie_info)
-
+        let {movie_info, review, user} = this.props.location.state
+        let slug_name = movie_info.movie_name.toLowerCase().split(' ').join('-')
         return (
+            <div>
+                <Header/>
+                <div className="movie-page">
 
-            <div className="movie-page">
-
-                <div className="movie-name">
-                    movie_name
-                </div>
-
-                <br/>
-
-                <div>
-                    <Space size={50}>
-                        <Image height={280} width={200} src={imagesMap.rango}/>
-                        <Descriptions bordered size={"small"} column={1}>
-                            <Descriptions.Item label="Director">director</Descriptions.Item>
-                            <Descriptions.Item label="Actor">leading_actor</Descriptions.Item>
-                            <Descriptions.Item label="Type"><a href="">category</a></Descriptions.Item>
-                            <Descriptions.Item label="Country of Production">country_of_production</Descriptions.Item>
-                            <Descriptions.Item label="Language">language</Descriptions.Item>
-                            <Descriptions.Item label="Release Date">release_date</Descriptions.Item>
-                            <Descriptions.Item label="Length">length</Descriptions.Item>
-                            <Descriptions.Item label="Rating">
-                                <Space>
-                                    rating
-                                    <Rate disabled defaultValue={3}/>
-                                </Space>
-                            </Descriptions.Item>
-                        </Descriptions>
-                    </Space>
-                </div>
-
-                <br/>
-
-                <div>
-                    <div className="introduction-header">
-                        Introduction
+                    <div className="movie-name">
+                        {movie_info.movie_name}
                     </div>
-                    <Card>
-                        <p>introduction</p>
-                    </Card>
-                </div>
 
-                <br/>
+                    <br/>
 
-                <div className="review-header">
-                    Reviews ( {data.length} )
-                </div>
-
-                <div>
-                    <List
-                        className="comment-list"
-                        itemLayout="horizontal"
-                        dataSource={data}
-                        renderItem={item => (
-                            <li>
-                                <Card>
+                    <div>
+                        <Space size={50}>
+                            <Image height={280} width={200} src={movie_info.image}/>
+                            <Descriptions bordered size={"small"} column={1}>
+                                <Descriptions.Item label="Director">{movie_info.director}</Descriptions.Item>
+                                <Descriptions.Item label="Actor">{movie_info.leading_actor}</Descriptions.Item>
+                                <Descriptions.Item
+                                    label="Country of Production">{movie_info.country_of_production}</Descriptions.Item>
+                                <Descriptions.Item label="Language">{movie_info.language}</Descriptions.Item>
+                                <Descriptions.Item label="Release Date">{movie_info.release_date}</Descriptions.Item>
+                                <Descriptions.Item label="Length">{movie_info.length}</Descriptions.Item>
+                                <Descriptions.Item label="Rating">
                                     <Space>
-                                        Rating: {item.rating}
-                                        <Rate disabled defaultValue={item.rating}/>
+                                        rating
+                                        <Rate disabled defaultValue={movie_info.rating}/>
                                     </Space>
-                                    <Comment
-                                        avatar={<Avatar
-                                            src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-                                            alt="Han Solo"/>}
-                                        author={item.user}
-                                        content={item.content}
-                                        actions={[
-                                            <Tooltip key="comment-like" title="Like">
-                    <span>
-                      {/* 未实现点赞功能 onClick=... */}
-                        {createElement(item.action === 'liked' ? LikeFilled : LikeOutlined)}
-                        <span className="comment-action">{item.likes}</span>
-                    </span>
-                                            </Tooltip>
-                                        ]}
-                                    />
-                                </Card>
-                            </li>
-                        )}
-                    />
+                                </Descriptions.Item>
+                            </Descriptions>
+                        </Space>
+                    </div>
+
+                    <br/>
+
+                    <div>
+                        <div className="introduction-header">
+                            Introduction
+                        </div>
+                        <Card>
+                            <p>{movie_info.introduction}</p>
+                        </Card>
+                    </div>
+
+                    <br/>
+
+                    <div className="review-header">
+                        Reviews ( {review.length} )
+                    </div>
+
+                    <div>
+                        <List
+                            className="comment-list"
+                            itemLayout="horizontal"
+                            dataSource={review}
+                            renderItem={(item, index) => (
+                                <li>
+                                    <Card>
+                                        <Space>
+                                            Rating: {item.rating}
+                                            <Rate disabled defaultValue={item.rating}/>
+                                        </Space>
+                                        <Comment
+                                            avatar={<Avatar
+                                                src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+                                                alt="Han Solo"/>}
+                                            author={user[index]}
+                                            content={item.content}
+                                            actions={[
+                                                <Tooltip key="comment-like" title="Like">
+                                                    <span onClick={() => this.addLikes(index, movie_info.movie_name)}>
+                                                        {createElement(item.action === 'liked' ? LikeFilled : LikeOutlined)}
+                                                        &nbsp;<span
+                                                        className="comment-action likes-number">{item.likes}</span>
+                                                    </span>
+                                                </Tooltip>
+                                            ]}
+                                        />
+                                    </Card>
+                                </li>
+                            )}
+                        />
+                    </div>
+
+                    <br/>
+
+                    <Button className="add-review-button" onClick={() => this.goToAddReviewPage(slug_name)}>
+                        + Review
+                    </Button>
+
                 </div>
-
-                <br/>
-
-                <Button className="add-review-button" type="primary" href="/add-review">
-                    + Review
-                </Button>
-
             </div>
         )
     }
